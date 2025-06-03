65dd49b859c889de61abdcf3ab05131e523f26a8
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-02 21:07:22 -0400

    added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <div className="navbar">
+            <div className="logo">
+                <h1>Cummings Maple</h1>
+            </div>
             <div className="links">
                 <Link to="/">Products</Link>
                 <Link to="/About">About Us</Link>

bb47978d62f73645817d2ad0e57702f432b174ef
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-03 23:42:46 -0400

    added more information for each product in data file

--- src/Products.js
+++ src/Products.js
@@ -2,76 +2,11 @@ const products = [
     {
         id: 1,
         name: "maple coated peanuts",
-        category: "maple coated peanuts",
-        family: "other",
+        category: "other",
+        family: "maple coated peanuts",
         size: "4.8oz",
         price: 5.00,
         description: "Dry roasted peanuts and pure maple syrup" 
     },
-    {
-        id: 2,
-        name: "maple coated peanuts",
-        category: "other",
-        sizes: ["4.8oz"],
-        prices: [5.00],
-        description: "A container of maple cotton candy" 
-    },
-    {
-        id: 3,
-        name: "maple candy",
-        category: "other",
-        sizes: ["single", "4-pack", "6-pack"],
-        prices: [5.00, 10.00, 15.00],
-        description: "Pure maple syrup in candy form" 
-    },
-    {
-        id: 4,
-        name: "maple coated peanuts",
-        category: "other",
-        sizes: ["4.8oz", "10oz"],
-        prices: [5.00, 10.00],
-        description: "Dry roasted peanuts and pure maple syrup" 
-    },
-    {
-        id: 5,
-        name: "maple coated peanuts",
-        category: "other",
-        sizes: ["4.8oz", "10oz"],
-        prices: [5.00, 10.00],
-        description: "Dry roasted peanuts and pure maple syrup" 
-    },
-    {
-        id: 6,
-        name: "maple coated peanuts",
-        category: "other",
-        sizes: ["4.8oz", "10oz"],
-        prices: [5.00, 10.00],
-        description: "Dry roasted peanuts and pure maple syrup" 
-    },
-    {
-        id: 7,
-        name: "maple coated peanuts",
-        category: "other",
-        sizes: ["4.8oz", "10oz"],
-        prices: [5.00, 10.00],
-        description: "Dry roasted peanuts and pure maple syrup" 
-    },
-    {
-        id: 8,
-        name: "maple syrup jug",
-        category: "syrup",
-        sizes: ["half-pint", "pint", "quart", "half-gallon", "gallon"],
-        grades: ["amber", "dark", "extra dark"],
-        prices: [5.00, 10.00, 15.00, 20.00, 25.00],
-        description: "A jug of maple syrup" 
-    },
-    {
-        id: 9,
-        name: "maple syrup glass leaf",
-        category: "syrup",
-        sizes: ["2oz", "4oz"],
-        grades: ["amber"],
-        prices: [5.00, 10.00],
-        description: "A glass leaf of maple syrup" 
-    },
+    
 ]

ade59b856fe5777233ffbaa04d585c861b445f0e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-03 23:11:03 -0400

    created products data file

---
+++ src/Products.js
@@ -0,0 +1,67 @@
+const products = [
+    {
+        id: 1,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 2,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 3,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 4,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 5,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 6,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 7,
+        name: "maple coated peanuts",
+        category: "other",
+        sizes: ["4.8oz", "10oz"],
+        prices: [5.00, 10.00],
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 8,
+        name: "maple syrup jug",
+        category: "syrup",
+        sizes: ["half-pint", "pint", "quart", "half-gallon", "gallon"],
+        grades: ["amber", "dark", "extra dark"],
+        prices: [5.00, 10.00, 15.00, 20.00, 25.00],
+        description: "A jug of maple syrup" 
+    },
+]

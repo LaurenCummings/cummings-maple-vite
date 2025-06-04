1da6c5170d000642f07106e5659633e619e924c4
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-03 23:48:14 -0400

    added more data to product file

--- src/Products.js
+++ src/Products.js
@@ -8,5 +8,49 @@ const products = [
         price: 5.00,
         description: "Dry roasted peanuts and pure maple syrup" 
     },
-    
+    {
+        id: 2,
+        name: "maple coated peanuts",
+        category: "other",
+        family: "maple coated peanuts",
+        size: "8oz",
+        price: 10.00,
+        description: "Dry roasted peanuts and pure maple syrup" 
+    },
+    {
+        id: 3,
+        name: "single maple candy",
+        category: "other",
+        family: "maple candy",
+        size: "2oz",
+        price: 5.00,
+        description: "A single piece of maple candy in the shape of a maple leaf" 
+    },
+    {
+        id: 4,
+        name: "maple candy pack",
+        category: "other",
+        family: "maple candy",
+        size: "4.8oz",
+        price: 10.00,
+        description: "A 4-pack of maple candies shaped like maple leaves" 
+    },
+    {
+        id: 5,
+        name: "maple candy pack",
+        category: "other",
+        family: "maple candy",
+        size: "8.8oz",
+        price: 15.00,
+        description: "An 8-pack of maple candies shaped like maple leaves" 
+    },   
+    {
+        id: 6,
+        name: "maple candy pack",
+        category: "other",
+        family: "maple candy",
+        size: "4.8oz",
+        price: 10.00,
+        description: "A 4-pack of maple candies in an assortment of shapes" 
+    },   
 ]

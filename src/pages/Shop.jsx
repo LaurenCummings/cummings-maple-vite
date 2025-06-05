80e9d5e2ab20f032b797ea17604296767f1ba751
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-05 00:48:44 -0400

    added data filter

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -8,20 +8,21 @@ function Shop() {
     const handleChange = (event) => {
         setSelectedProducts(event.target.value);
     }
-    console.log(selectedProducts);
+    
+    const filteredData = products.filter((item) => item.category === selectedProducts || item.family === selectedProducts);
 
     return (
         <div className="shop">
             <h1>Our Products</h1>
             <select name="filter" onChange={handleChange}>
                 <option value="all">All Products</option>
-                <option value="peanuts">Peanuts</option>
-                <option value="candy">Candy</option>
+                <option value="maple coated peanuts">Peanuts</option>
+                <option value="maple candy">Candy</option>
                 <option value="syrup">Syrup</option>
                 <option value="cotton-candy">Cotton Candy</option>
             </select>
             <div className="products">
-                {products.map((item) => {
+                {filteredData.map((item) => {
                     return (
                         <div key={item.id} className="product">
                             <img src={item.image} alt={item.name} />

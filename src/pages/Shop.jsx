cf4e6e4741dd89a28fa6881f75794a01df940eb1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-04 23:50:07 -0400

    added div around products grid and added title

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,16 +3,19 @@ import products from '../Products';
 
 function Shop() {
     return (
-        <div className="products">
-            {products.map((item) => {
-                return (
-                    <div key={item.id} className="product">
-                        <img src={item.image} alt={item.name} />
-                        <h2>{item.name}</h2>
-                        <p>${item.price}</p>
-                    </div>
-                )
-            })}
+        <div className="shop">
+            <h1>Our Products</h1>
+            <div className="products">
+                {products.map((item) => {
+                    return (
+                        <div key={item.id} className="product">
+                            <img src={item.image} alt={item.name} />
+                            <h2>{item.name}</h2>
+                            <p>${item.price}</p>
+                        </div>
+                    )
+                })}
+            </div>            
         </div>
     )
 }

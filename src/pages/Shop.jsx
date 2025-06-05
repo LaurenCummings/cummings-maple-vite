df2351e0e01aac63f926902b73f09ab520ab40c0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-05 00:36:39 -0400

    added select element with options

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,6 +5,13 @@ function Shop() {
     return (
         <div className="shop">
             <h1>Our Products</h1>
+            <select name="filter">
+                <option value="all">All Products</option>
+                <option value="peanuts">Peanuts</option>
+                <option value="candy">Candy</option>
+                <option value="syrup">Syrup</option>
+                <option value="cotton-candy">Cotton Candy</option>
+            </select>
             <div className="products">
                 {products.map((item) => {
                     return (

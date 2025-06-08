47e7a58e7d261e34b03a865268cc7bd03f35fec5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-07 23:00:11 -0400

    changed product page to be 2 columns

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -9,19 +9,25 @@ function ProductPage() {
     return (
         <div className="product-page">
             <h1>{product.name}</h1>
-            <img src={product.image} alt={product.name} />
-            <p>{product.size}</p>
-            <p>${product.price.toFixed(2)}</p>
-            <p>{product.description}</p>
-            { product.grades && 
-                <p>Available in </p>
-            }
-            <div className="product-grades">
-                { product.grades && product.grades.map((grade, index) => {
-                    return (
-                        <GradeLabel key={index} grade={grade} />                  
-                    )    
-                })}
+            <div className="product-container">
+                <div className="product-image">
+                    <img src={product.image} alt={product.name} />   
+                </div>
+                <div className="product-info">
+                    <p>{product.size}</p>
+                    <p>${product.price.toFixed(2)}</p>
+                    <p>{product.description}</p>
+                    { product.grades && 
+                        <p>Available in </p>
+                    }
+                    <div className="product-grades">
+                        { product.grades && product.grades.map((grade, index) => {
+                            return (
+                                <GradeLabel key={index} grade={grade} />                  
+                            )    
+                        })}
+                    </div>                
+                </div>                
             </div>
         </div>
     )

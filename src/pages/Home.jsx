ccdec43b5dcadee5e2ac1f426a3b5134c6e798b2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-10 23:51:18 -0400

    added bucket to home page and added classNames to images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import '../css/Home.css';
 import trees from '../assets/trees.jpg';
+import bucket from '../assets/bucket.jpg';
 
 function Home() {
     return (
         <div className="home">
-            <img src={trees} alt="trees" />
+            <img className="banner" src={trees} alt="trees" />
+            <div className="home-about-us">
+                <img className="home-about-us-img" src={bucket} alt="bucket" />
+                <h2>View Our Products</h2>
+            </div>
+            <div className="home-products">
+
+            </div>
+            <div className="home-contact-us">
+
+            </div>
         </div>
     )
 }

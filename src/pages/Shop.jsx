9d780bba6d176eff8808eaff7185cc0fcd411573
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-11 23:35:57 -0400

    added note about shipping to shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -23,6 +23,7 @@ function Shop() {
     return (
         <div className="shop">
             <h1>Our Products</h1>
+            <p>Available for shipment. Please call for pricing.</p>
             <select name="filter" onChange={handleChange}>
                 <option value="all">All Products</option>
                 <option value="syrup">Syrup</option>

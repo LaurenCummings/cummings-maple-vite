248e66fd6023fb9140b4b951848ba47bcfbd814e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-18 22:58:20 -0400

    added classname for styling

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,7 @@ import '../css/ContactPage.css';
 
 function ContactPage() {
     return (
-        <div>
+        <div className="contact-page">
             <h1>Contact Us</h1>
             <p>If you have questions or would like to stop by, please contact us at one of the following:</p>
             <p>Phone: (315) 576-0203</p>

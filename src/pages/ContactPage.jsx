34ff709de3f3bd71489c29ed00e56f63f3a78084
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-18 22:57:26 -0400

    added content to contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,7 +3,10 @@ import '../css/ContactPage.css';
 function ContactPage() {
     return (
         <div>
-            ContactPage
+            <h1>Contact Us</h1>
+            <p>If you have questions or would like to stop by, please contact us at one of the following:</p>
+            <p>Phone: (315) 576-0203</p>
+            <p>Email: melanie.cummings@att.net</p>
         </div>
     )
 }

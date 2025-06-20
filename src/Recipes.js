e3020fd730418920dba413ecde388db99348dda3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-19 23:32:00 -0400

    added trailing commas to recipes entries

--- src/Recipes.js
+++ src/Recipes.js
@@ -3,13 +3,13 @@ const recipes = [
         id: 1,
         name: "No Bake Maple Cheesecake or Dip",
         ingredients: ["1/2 cup Granulated Maple Sugar", "8 oz. Cream Cheese - softened", "8 oz. Cool Whip"],
-        instructions: ["Mix maple sugar and cream cheese with electric mixer until well blended. Mix in cool whip. Pour in a pie shell if making pie. Refrigerate at least 8 hrs to let flavors blend. Top pie or tarts with fruit, nuts, or additional maple sugar if desired. Serve as a dip with pretzels, graham crackers, vanilla wafers, apple slices, or strawberries. Enjoy!"]
+        instructions: ["Mix maple sugar and cream cheese with electric mixer until well blended. Mix in cool whip. Pour in a pie shell if making pie. Refrigerate at least 8 hrs to let flavors blend. Top pie or tarts with fruit, nuts, or additional maple sugar if desired. Serve as a dip with pretzels, graham crackers, vanilla wafers, apple slices, or strawberries. Enjoy!"],
     },
     {
         id: 2,
         name: "Maple Chicken Wings",
         ingredients: ["40-50 chicken wing sections", "1/2 cup very dark maple syrup", "1/4 cup brown sugar", "1/4 tsp pepper", "1-1/2 tsp cornstarch", "1/4 cup butter", "1/2 cup Frank's Hot Sauce", "1/2 tsp salt", "2 Tbsp cold water"],
-        instructions: ["Broil wing sections until brown and crispy (turn part way through cooking). Can also grill.", "In a small saucepan, melt butter. Stir in the syrup, brown sugar, hot sauce, salt, and pepper. Combine water and cornstarch. Stir into sauce. Bring to a boil. Cook and stir for 2-5 minutes, or until thickened. Place wings in a large bowl and toss with sauce to coat. Serve with blue cheese or ranch dressing. Enjoy!"]
+        instructions: ["Broil wing sections until brown and crispy (turn part way through cooking). Can also grill.", "In a small saucepan, melt butter. Stir in the syrup, brown sugar, hot sauce, salt, and pepper. Combine water and cornstarch. Stir into sauce. Bring to a boil. Cook and stir for 2-5 minutes, or until thickened. Place wings in a large bowl and toss with sauce to coat. Serve with blue cheese or ranch dressing. Enjoy!"],
     },
 ]
 

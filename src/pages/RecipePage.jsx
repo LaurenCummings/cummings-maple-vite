e02c08711821522dedc2472b031696841ddb4891
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-19 23:49:40 -0400

    started printing recipes

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,10 +1,22 @@
 import '../css/RecipePage.css';
+import recipes from '../Recipes.js';
 
 function RecipePage() {
     return (
         <div className="recipe-page">
             <h1>Recipes</h1>
             <h3>Here are some of our favorite maple recipes</h3>
+            {
+                recipes.map((recipe) => {
+                    return (
+                        <div>
+                            <h2>{recipe.name}</h2>
+                            <p>{recipe.ingredients}</p>
+                            <p>{recipe.instructions}</p>                            
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }

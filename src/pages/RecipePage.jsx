efcdd2028526eb00ab1ee83cd9a6ec03c4709531
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-19 23:38:24 -0400

    added some content to recipe page

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -2,8 +2,9 @@ import '../css/RecipePage.css';
 
 function RecipePage() {
     return (
-        <div>
-            RecipePage
+        <div className="recipe-page">
+            <h1>Recipes</h1>
+            <p>Here are some of our favorite maple recipes</p>
         </div>
     )
 }

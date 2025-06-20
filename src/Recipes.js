71ba953b24d7083b1af7b08c49b2bc49aea31d1f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-19 23:23:51 -0400

    added maple cheesecake dip to recipes

---
+++ src/Recipes.js
@@ -0,0 +1,13 @@
+const recipes = [
+    {
+        id: 1,
+        name: "No Bake Maple Cheesecake or Dip",
+        ingredients: ["1/2 cup Granulated Maple Sugar", "8 oz. Cream Cheese - softened", "8 oz. Cool Whip"],
+        instructions: "Mix maple sugar and cream cheese with electric mixer until well blended. Mix in cool whip. Pour in a pie shell if making pie. Refrigerate at least 8 hrs to let flavors blend. Top pie or tarts with fruit, nuts, or additional maple sugar if desired. Serve as a dip with pretzels, graham crackers, vanilla wafers, apple slices, or strawberries. Enjoy!"
+    },
+    {
+
+    },
+]
+
+export default recipes;

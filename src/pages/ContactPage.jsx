a77809396dd4a64e2d815766c223a7e60a1dbfc6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-06-28 23:58:39 -0400

    added address to contact page and styled

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,14 +4,14 @@ function ContactPage() {
     return (
         <div className="contact-page">
             <h1>Contact Us</h1>
-            <p>We are located at:</p>
+            <p>If you have questions or would like to stop by, please contact us at one of the following:</p>
+            <p>Phone: (315) 576-0203</p>
+            <p>Email: melanie.cummings@att.net</p>
+            <p>Our address is:</p>
             <div className="address">
                 <p>2814 Harris Rd</p>
                 <p>Palmyra, NY</p>    
             </div>
-            <p>If you have questions or would like to stop by, please contact us at one of the following:</p>
-            <p>Phone: (315) 576-0203</p>
-            <p>Email: melanie.cummings@att.net</p>
         </div>
     )
 }
